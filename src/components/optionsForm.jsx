301e22ccf8560a465c244d033b30dbb624ea8152
asar17 <aelhaidary2022@gmail.com>
2024-06-30 19:06:29 +0300

    end

--- src/components/optionsForm.jsx
+++ src/components/optionsForm.jsx
@@ -32,7 +32,7 @@ const OptionsForm = ({option,selectOption}) =>{
                                                 "B2C - Weekday (Selling)":"00.00",
                                                 "B2C - Weekend (Selling)":"00.00",
 
-                                        }]);  setDatePicker(false); setRefresh("")}}  
+                                        }]);  setDatePicker(false);}}  
                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                                  /> 
                                 <label htmlFor={op}>{op}</label>
